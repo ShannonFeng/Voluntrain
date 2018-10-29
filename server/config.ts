71677e35ae0c6a50797feffda07ad184d7340003
Shannon Feng <16shannon.feng@gmail.com>
2018-10-28 19:23:26 -0500

    updated server.ts file with test connection string

---
+++ server/config.ts
@@ -0,0 +1,3 @@
+module.exports = {
+    db: `mongodb+srv://testuser:Voluntrain1@cluster0-owfie.mongodb.net/test?retryWrites=true`
+}
--- server/server.ts
+++ server/server.ts
@@ -1,11 +1,21 @@
 var express = require("express");
+var MongoClient = require("mongodb").MongoClient;
 
 const app = express();
 
 app.listen(8000, () => {
     console.log('Voluntrain server started on http://localhost:8000');
+    
+    var uri = "mongodb+srv://testuser:Voluntrain1@cluster0-owfie.mongodb.net/test?retryWrites=true";
+    MongoClient.connect(uri, function(err, client) {
+      const collection = client.db("test").collection("devices");
+      console.log("Connected to the cluster.");
+      // perform actions on the collection object
+      client.close();
+    });
   });
 
 app.get('/', function (req, res) {
-  res.send('Hello World! This is the home route.')
+    console.log("User on home route.");
+    res.send("Hello");
 })
