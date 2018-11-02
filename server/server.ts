ffd8d77241ca0adb3b10471e8923fd3be7133136
NATHAN CHRISTOPHER KIRALY <nkiraly@wisc.edu>
2018-11-01 21:43:37 -0500

    Added functionality for user to create their account

--- server/server.ts
+++ server/server.ts
@@ -6,10 +6,15 @@ const app = express();
 app.use(express.static("dist/Voluntrain"));
 var uri = "mongodb://testuser:Voluntrain1@voluntrain-shard-00-00-owfie.mongodb.net:27017,voluntrain-shard-00-01-owfie.mongodb.net:27017,voluntrain-shard-00-02-owfie.mongodb.net:27017/test?ssl=true&replicaSet=Voluntrain-shard-0&authSource=admin&retryWrites=true";
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+})
+
 app.listen(3000, () => {
     console.log('Voluntrain server started on http://localhost:3000');
     MongoClient.connect(uri, function(err, client) {
-      const collection = client.db("test").collection("devices");
       console.log("Connected to the cluster.");
       // perform actions on the collection object
       client.close();
@@ -48,5 +53,22 @@ app.get('/test/:eventName', function (req, res) {
       db.close();
     });
     */
+  });
+})
+
+app.get('/createaccount/', function (req, res) {
+
+  MongoClient.connect(uri, function(err, db) {
+    if (err) throw err;
+    var dbo = db.db("Voluntrain");
+
+    var newUser = {name: req.query.name, email: req.query.email, zipcode: req.query.zipcode, password: req.query.password };
+
+    dbo.collection("Users").insertOne(newUser , function(err, result) {
+      if (err) throw err;
+      console.log(result);
+      res.send(result);
+      db.close();
+    });
   });
 })
--- src/app/createaccount/createaccount.component.ts
+++ src/app/createaccount/createaccount.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import {HttpParams} from "@angular/common/http";
 
 @Component({
   selector: 'app-createaccount',
@@ -6,10 +9,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./createaccount.component.css']
 })
 export class CreateaccountComponent implements OnInit {
+  signupForm; 
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  constructor(private formBuilder: FormBuilder, private http:HttpClient) {
+    this.signupForm = formBuilder.group({
+      name: '',
+      email: '',
+      zipcode: '',
+      password: ''
+    });
   }
+
+  submit() {
+    console.log("On signup")
+
+    const params = new HttpParams()
+    .set('name', this.signupForm.value.name)
+    .set('email', this.signupForm.value.email)
+    .set('zipcode', this.signupForm.value.zipcode)
+    .set('password', this.signupForm.value.password);
+
+    this.http.get("http://localhost:3000/createaccount", {params}).subscribe (
+      data => { 
+        console.log("here");
+      },
+      err => {
+        console.log("there was an error")
+      },
+      () => console.log("done")
+    )
+  }
+
+
+
+  
+
+
 
 }
