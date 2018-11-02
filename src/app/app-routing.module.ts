567616aed725deee88041d777943d5bcef46ef06
Mirza Nor Azman <spellhaikarl@gmail.com>
2018-11-02 15:53:35 -0500

    Added Org Form component and adjusted routing modules for nav-bar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { RouterModule, Routes}  from '@angular/router';
 import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
 import { HomeComponent } from './home/home.component';
 import { CreateaccountComponent } from './createaccount/createaccount.component';
+import { OrgFormComponent } from './org-form/org-form.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent},
   { path: 'user-login-form', component: UserLoginFormComponent},
   { path: 'createaccount', component: CreateaccountComponent}
+  { path: 'org-form', component: OrgFormComponent}
 ];
 
 @NgModule({
--- src/app/org-form/org-form.component.ts
+++ src/app/org-form/org-form.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { toArray } from 'rxjs/operators';
 
 @Component({
   selector: 'app-org-form',
   templateUrl: './org-form.component.html',
   styleUrls: ['./org-form.component.css']
 })
-export class OrgFormComponent implements OnInit {
+export class OrgFormComponent  {
+  orgForm;   // will be used for FormGroup instance
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  constructor(private formBuilder: FormBuilder, private http:HttpClient) {
+    this.orgForm = formBuilder.group({
+      orgName: '',
+      address: '',
+      zipcode: '',
+      bio: ''
+    });
+  }
+
+  submit() {
+    //unimplemented
   }
 
 }
