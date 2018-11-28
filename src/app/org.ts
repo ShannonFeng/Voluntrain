1efe5a6c1402993a77faa9dfffdb911d445f9d41
Mirza Nor Azman <spellhaikarl@gmail.com>
2018-11-28 05:59:51 -0600

    initializing orgs component

---
+++ src/app/org.ts
@@ -0,0 +1,2 @@
+export class Org {
+}
---
+++ src/app/view-org/view-org.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-view-org',
+  templateUrl: './view-org.component.html',
+  styleUrls: ['./view-org.component.css']
+})
+export class ViewOrgComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
