bb850b6c89fa8ad4b48a4a3cf7ba556f56db737e
James Pratt <jpratt@Jamess-MacBook-Air-2.local>
2018-11-30 16:20:54 -0600

    fixed event component map

--- src/app/view-event/view-event.component.ts
+++ src/app/view-event/view-event.component.ts
@@ -7,13 +7,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ViewEventComponent implements OnInit {
 
+  lat = 43.0746953;
+  lng = -89.3841695;
+  zoom = 14;
+  
   constructor() { }
 
   eventName;
   address;
   volunteers;
 
+  getLocation(): void{
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition((position)=>{
+          this.lng = position.coords.longitude;
+          this.lat = position.coords.latitude;
+        });
+    } else {
+       console.log("No support for geolocation");
+       this.lng =-89.3841695;
+       this.lat =43.0746953;
+    }
+  }
   ngOnInit() {
+    this.getLocation();
     this.eventName = "Event1";
     this.address = "1234"; 
     this.volunteers = ["Bob", "Catherine", "Dan"];
